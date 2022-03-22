2a6f26dcbc084e18d6dbc9100d408f3733d08576
mohammad <mr.dori.dev@gmail.com>
2022-03-22 18:21:41 +0430

    set postgresql database with docker

--- dblog/settings.py
+++ dblog/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'dblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'data',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
