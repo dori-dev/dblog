ecf673e9c1ad649f2e5e48248c8348465c99b3db
mohammad <mr.dori.dev@gmail.com>
2022-03-25 18:47:30 +0430

    add django rest framework to project

--- dblog/urls.py
+++ dblog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('', include('blog.urls')),
+    path('api-auth/', include('rest_framework.urls')),
 ]
 
 if settings.DEBUG:
